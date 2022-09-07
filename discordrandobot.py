478db332a4a4030ae57ac51e8e8ec75240476f9b
stoupare <alexander.stouparenko@gmail.com>
2022-09-06 21:51:42 -0400

    slash support for shrekbot

--- discordrandobot.py
+++ discordrandobot.py
@@ -1,58 +1,50 @@
 import discord
+from discord import app_commands
 import random
 import os
 
-client = discord.Client()
-rand_prefix = "!rand"
-roll_prefix = "!roll"
-rollEnd_prefix = "!rollend"
+intents = discord.Intents.default()
+intents.message_content = True
 
-random.seed()
-USAGE_RAND_MSG = "Usage: !rand [startNum] [endNum]"
-USAGE_ROLL_MSG = "Usage: !roll [description] ------> !rollend [rollID] to finalize"
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-def safe_cast(val, to_type, default=None):
-    try:
-        return to_type(val)
-    except (ValueError, TypeError):
-        return default
 
-def safe_args(args):
-    for arg in args:
-        if (safe_cast(arg, int) == None):
-            return False
-    return True
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
 
-def clean_input(inp):
-    tempinp = [];
-    if (len(inp) > 0):
-        for x in inp:
-            if x.strip():
-                print(x)
-                tempinp.append(x)
+    async def setup_hook(self):
+        await self.tree.sync()
 
-    return tempinp
 
-async def randFunc(message):
-    args = message.content[len(rand_prefix):].strip().split(' ')
-    args = clean_input(args)
-    text = ""
-    if len(args) == 2:
-        for arg in args:
-            if (safe_args(args)):
-                text = random.randint(safe_cast(args[0], int), safe_cast(args[1], int))
-            else:
-                text = USAGE_RAND_MSG 
-    elif len(args) == 1:
-        if (safe_args(args)):
-            text = random.randint(0, safe_cast(args[0], int))
-        else:                
-            text = USAGE_RAND_MSG
-    elif len(args) == 0:
-        text = random.randint(0,100)
-    else:
-        text = USAGE_RAND_MSG
-    await message.channel.send(text)
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+        
+
+class ButtonView(discord.ui.View):
+    @discord.ui.button(label='Start group roll!', style=discord.ButtonStyle.primary)
+    async def roll(self, interaction: discord.Interaction, button: discord.ui.Button):
+        final_message = await interaction.message.fetch()
+        await rollEndFunc(final_message)
+        button.disabled = True
+        button.label = "Roll Complete!"
+        button.style = discord.ButtonStyle.secondary
+        await interaction.response.edit_message(view=self)
+
+
+def init_insults():
+    insults = []
+    f = open("insults.txt", "r+")
+    for l in f:
+        insults.append(l)
+    return insults
+
+async def randFunc(startNum, endNum):
+    text = random.randint(startNum, endNum)
+    print(text)
+    return text
 
 async def rollFunc(message):
     args = message.content[len(roll_prefix):].strip()
@@ -65,59 +57,77 @@ async def rollFunc(message):
     await roll_message.add_reaction("🎲")
     await message.channel.send("**Use This Roll ID**:\n > " + str(roll_message.id))
 
-async def rollEndFunc(message, client):
-    args = message.content[len(rollEnd_prefix):].strip()
-    parts = args.split(' ')
-    print(args, parts)
-    msg_id = -1
-    if (len(clean_input(parts)) == 1):
-        try:
-            msg_id = int(args)
-        except (ValueError, TypeError):
-            mes_id = -1
-    if (msg_id == -1):
-        await message.channel.send("Missing/Invalid ID")
-    else:    
-        updated_msg = await message.channel.fetch_message(msg_id)
-        users = await updated_msg.reactions[0].users().flatten()
+async def rollEndFunc(message):
+    users = [ user async for user in message.reactions[0].users()]
 
-        results = []
-        for u in range(0, len(users)):
-            if (users[u] != client.user):
-                val = random.randint(0,99)
-                results.append((users[u].mention, val))
-                await message.channel.send(users[u].mention + " Rolled a " + str(val))
+    results = []
+    for u in range(0, len(users)):
+        if (users[u] != client.user):
+            val = random.randint(0,99)
+            results.append((users[u].mention, val))
+            await message.channel.send(users[u].mention + " Rolled a " + str(val))
 
-        winner = ("",0)
-        for fin in range(0, len(results)):
-            if (results[fin][1] > winner[1]):
-                winner = results[fin]
-        print("winner", winner)
-        await message.channel.send(winner[0] + " Wins with a " + str(winner[1]))
+    winner = ("",0)
+    for fin in range(0, len(results)):
+        if (results[fin][1] > winner[1]):
+            winner = results[fin]
+    print("winner", winner)
+    await message.channel.send(winner[0] + " Wins with a " + str(winner[1]))
+
+
+async def rollFunc(message):
+    parts = message.split('d')
+    message = "You rolled: "
+    print(parts)
+    total = 0
+    for i in range(int(parts[0])):
+        result = random.randint(0, int(parts[1]))
+        message += str(result)
+        total += result
+        if (i + 1 != parts[0]):
+            message +=","
+
+    if (int(parts[0]) > 1):
+        message += "\n Total of {0}".format(total)
+    return message
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    inp = message.content.strip().split(' ')
-    print("input: ", inp)
-    if (len(inp) >= 1 and inp[0].find("!") != -1):
-        command = inp[0]
-        print("command: ", command)
-        if (command == rand_prefix):
-            await randFunc(message)
-        elif (command == roll_prefix):
-            await rollFunc(message)
-        elif (command == rollEnd_prefix):
-            await rollEndFunc(message, client)
-        elif (command == "!shrek_help"):
-            await message.channel.send("Ey Donkeh! This is easy to use! \n> " + USAGE_RAND_MSG + "\n> " + USAGE_ROLL_MSG)
-            
+@client.tree.command()
+async def vm(interaction: discord.Interaction):
+    """performs vicous mockery"""
+    insult = insults[random.randint(0, len(insults))]
+    await interaction.response.send_message(f'{insult}')
+
+
+@client.tree.command(name="random")
+@app_commands.describe(
+    startnum='start value',
+    endnum='end value',
+)
+async def randnum(interaction: discord.Interaction, startnum: int=0, endnum: int=100):
+    """random number gen"""
+    res = await randFunc(startnum, endnum)
+    await interaction.response.send_message(res)
     
-client.run(os.getenv('TOKEN'))
+@client.tree.command()
+@app_commands.describe(
+    die="the amount of dice you want rolled. ie: 2d20, 1d6, 3d100"
+)
+async def roll(interaction: discord.Interaction, die: str='1d20'):
+    """rolls given dice"""
+    res = await rollFunc(die)
+    await interaction.response.send_message(res)
+
+@client.tree.command()
+async def grouproll(interaction: discord.Interaction):
+    await interaction.response.send_message("Here's your rolls", view = ButtonView())
+    resp = await interaction.original_response()
+    await resp.add_reaction("🎲")
+            
+insults = init_insults()
+client.run('ODAxMTc4NTM5OTc3OTk4Mzg3.YAc5qQ.jLs7vRG6YKBQF_orE8ISPqMA_bY')
 
